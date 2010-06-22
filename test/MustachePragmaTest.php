b096cb108d856b570fdc1bad234da0103ae17d9d
Justin Hileman <justin@justinhileman.info>
2010-06-22 07:13:17 -0400

    fix for PHPUnit 3.5.0 - no longer including PHPUnit/Framework.php in tests.

--- test/MustachePragmaTest.php
+++ test/MustachePragmaTest.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once '../Mustache.php';
-require_once 'PHPUnit/Framework.php';
 
 class MustachePragmaTest extends PHPUnit_Framework_TestCase {
 
--- test/MustachePragmaUnescapedTest.php
+++ test/MustachePragmaUnescapedTest.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once '../Mustache.php';
-require_once 'PHPUnit/Framework.php';
 
 class MustachePragmaUnescapedTest extends PHPUnit_Framework_TestCase {
 
