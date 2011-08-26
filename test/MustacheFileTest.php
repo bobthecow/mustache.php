45152be7bfd9cd85fd23df87060365ea5ca87409
Cameron Bytheway <bytheway.cameron@gmail.com>
2011-08-26 04:08:15 -0600

    Added recursive tests

--- test/MustacheFileTest.php
+++ test/MustacheFileTest.php
@@ -17,11 +17,11 @@ class MustacheFileTest extends PHPUnit_Framework_TestCase {
          * @return void
          */
         public function testRenderFile() {
-                $basedir = dirname(__FILE__) . '/../examples/';
+                $basedir = dirname(__FILE__) . '/../examples';
 
                 $m = new Mustache();
 
-                $this->assertEquals(file_get_contents($basedir . 'simple/simple.txt'), $m->renderFile($basedir . 'simple/simple.mustache'));
+                $this->assertEquals(file_get_contents($basedir . '/simple/simple.txt'), $m->renderFile($basedir . '/simple/simple.mustache'));
                 
                 try {
                        $m->renderFile(null);
@@ -30,7 +30,7 @@ class MustacheFileTest extends PHPUnit_Framework_TestCase {
                 }
                 
                 try {
-                       $m->renderFile($basedir.'bogus/bogus.mustache');
+                       $m->renderFile($basedir.'/bogus/bogus.mustache');
                 } catch (Exception $exc) {
                         $this->assertInstanceOf(InvalidArgumentException, $exc);
                 }
@@ -42,12 +42,30 @@ class MustacheFileTest extends PHPUnit_Framework_TestCase {
          * @access public
          * @return void
          */
-        public function testRenderPartialDir() {
-                $basedir = dirname(__FILE__) . '/../examples/';
+        public function testRenderPartialsInDir() {
+                $basedir = dirname(__FILE__) . '/../examples/file_render';
                 
                 $m = new Mustache();
                 
+                // Mustache sound be able to look in the same directory for the required partials
+                $this->assertEquals(file_get_contents($basedir . '/file_render.txt'), $m->renderFile($basedir.'/file_render.mustache'));
                 
+                // TODO test that Mustache throws an exception when it can't find any partials with that name
+        }
+        
+        /**
+         * Tests rendering a directory of partials recursively
+         * 
+         * @access public
+         * @return void
+         */
+        public function testRenderPartialsInDirRecursive() {
+                $basedir = dirname(__FILE__) . '/../examples/file_render_recursive';
+                
+                $m = new Mustache();
+                
+                // Mustache sound be able to look in the same directory for the required partials
+                $this->assertEquals(file_get_contents($basedir . '/file_render_recursive.txt'), $m->renderFile($basedir.'/file_render_recursive.mustache'), TRUE);
         }
 
 }
