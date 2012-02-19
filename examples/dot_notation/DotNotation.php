0cf96edfe5f83dab3fea76a7ad944bd2adebcab3
jwronsky <mateusz.charytoniuk@gmail.com>
2012-02-19 17:37:05 +0100

    * whitespace fix

--- examples/dot_notation/DotNotation.php
+++ examples/dot_notation/DotNotation.php
@@ -9,11 +9,11 @@ class DotNotation extends Mustache {
 	public $person = array(
 		'name' => array('first' => 'Chris', 'last' => 'Firescythe'),
 		'age' => 24,
-        'hobbies' => array('Cycling', 'Fishing'),
-        'hometown' => array(
-            'city'  => 'Cincinnati',
-            'state' => 'OH',
-        ),
+		'hobbies' => array('Cycling', 'Fishing'),
+		'hometown' => array(
+			'city'  => 'Cincinnati',
+			'state' => 'OH',
+		),
 	);
 
 	public $normal = 'Normal';
