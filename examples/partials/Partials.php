fe9fccb44162b302226bc70dd4aadbb1a1728ca8
Justin Hileman <justin@justinhileman.info>
2012-03-14 13:20:30 -0700

    Update the `partials` example to be a failing test case for partial indenting.

--- examples/partials/Partials.php
+++ examples/partials/Partials.php
@@ -1,9 +1,9 @@
 <?php
 
 class Partials {
-	public $name = 'ilmich';
-	public $data = array(
-		array('name' => 'federica', 'age' => 27, 'gender' => 'female'),
-		array('name' => 'marco', 'age' => 32, 'gender' => 'male'),
+	public $page = array(
+		'title'    => 'Page Title',
+		'subtitle' => 'Page Subtitle',
+		'content'  => 'Lorem ipsum dolor sit amet.',
 	);
 }
