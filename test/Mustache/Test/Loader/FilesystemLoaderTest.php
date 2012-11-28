1560863c49cbd97861b81e99fbbe8b253ccd41f5
Justin Hileman <justin@justinhileman.info>
2012-11-28 12:58:20 -0800

    Failing test for empty-string filesystem loader extension.
    See #123

--- test/Mustache/Test/Loader/FilesystemLoaderTest.php
+++ test/Mustache/Test/Loader/FilesystemLoaderTest.php
@@ -30,6 +30,19 @@ class Mustache_Test_Loader_FilesystemLoaderTest extends PHPUnit_Framework_TestCa
         $this->assertEquals('two contents', $loader->load('two.mustache'));
     }
 
+    public function testEmptyExtensionString()
+    {
+        $baseDir = realpath(dirname(__FILE__).'/../../../fixtures/templates');
+
+        $loader = new Mustache_Loader_FilesystemLoader($baseDir, array('extension' => ''));
+        $this->assertEquals('one contents', $loader->load('one.mustache'));
+        $this->assertEquals('alpha contents', $loader->load('alpha.ms'));
+
+        $loader = new Mustache_Loader_FilesystemLoader($baseDir, array('extension' => null));
+        $this->assertEquals('two contents', $loader->load('two.mustache'));
+        $this->assertEquals('beta contents', $loader->load('beta.ms'));
+    }
+
     /**
      * @expectedException RuntimeException
      */
