8b02366a3fdc91bc7180ca0e5b6da3b0f82cbd5e
Justin Hileman <justin@justinhileman.info>
2013-01-20 10:50:00 -0800

    Add test coverage for new exception types.

---
+++ test/Mustache/Test/Exception/UnknownHelperExceptionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of Mustache.php.
+ *
+ * (c) 2013 Justin Hileman
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Mustache_Test_Exception_UnknownHelperExceptionTest extends PHPUnit_Framework_TestCase
+{
+    public function testInstance()
+    {
+        $e = new Mustache_Exception_UnknownHelperException('alpha');
+        $this->assertTrue($e instanceof InvalidArgumentException);
+        $this->assertTrue($e instanceof Mustache_Exception);
+    }
+
+    public function testMessage()
+    {
+        $e = new Mustache_Exception_UnknownHelperException('beta');
+        $this->assertEquals('Unknown helper: beta', $e->getMessage());
+    }
+
+    public function testGetHelperName()
+    {
+        $e = new Mustache_Exception_UnknownHelperException('gamma');
+        $this->assertEquals('gamma', $e->getHelperName());
+    }
+}
