d6e1b4505377d5c803fb90eb1e87355262541a6f
Amit Snyderman <amit@amitsnyderman.com>
2013-09-16 20:17:39 -0400

    Add copyright/license header

--- src/Mustache/Cache/AbstractCache.php
+++ src/Mustache/Cache/AbstractCache.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Mustache.php.
+ *
+ * (c) 2012 Justin Hileman
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 abstract class Mustache_Cache_AbstractCache implements Mustache_Cache
 {
     private $logger = null;
