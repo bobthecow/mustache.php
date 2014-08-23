3f492a1bf54dc8b62dc42300d28b6ee06c47529a
Justin Hileman <justin@justinhileman.info>
2014-08-22 22:06:02 -0700

    Add a simple FILTERS pragma example.
    This includes examples of both section and interpolation filters.

---
+++ test/fixtures/examples/filters/Filters.php
@@ -0,0 +1,88 @@
+<?php
+
+class Filters
+{
+    public $states = array(
+        'al' => 'Alabama',
+        'ak' => 'Alaska',
+        'az' => 'Arizona',
+        'ar' => 'Arkansas',
+        'ca' => 'California',
+        'co' => 'Colorado',
+        'ct' => 'Connecticut',
+        'de' => 'Delaware',
+        'fl' => 'Florida',
+        'ga' => 'Georgia',
+        'hi' => 'Hawaii',
+        'id' => 'Idaho',
+        'il' => 'Illinois',
+        'in' => 'Indiana',
+        'ia' => 'Iowa',
+        'ks' => 'Kansas',
+        'ky' => 'Kentucky',
+        'la' => 'Louisiana',
+        'me' => 'Maine',
+        'md' => 'Maryland',
+        'ma' => 'Massachusetts',
+        'mi' => 'Michigan',
+        'mn' => 'Minnesota',
+        'ms' => 'Mississippi',
+        'mo' => 'Missouri',
+        'mt' => 'Montana',
+        'ne' => 'Nebraska',
+        'nv' => 'Nevada',
+        'nh' => 'New Hampshire',
+        'nj' => 'New Jersey',
+        'nm' => 'New Mexico',
+        'ny' => 'New York',
+        'nc' => 'North Carolina',
+        'nd' => 'North Dakota',
+        'oh' => 'Ohio',
+        'ok' => 'Oklahoma',
+        'or' => 'Oregon',
+        'pa' => 'Pennsylvania',
+        'ri' => 'Rhode Island',
+        'sc' => 'South Carolina',
+        'sd' => 'South Dakota',
+        'tn' => 'Tennessee',
+        'tx' => 'Texas',
+        'ut' => 'Utah',
+        'vt' => 'Vermont',
+        'va' => 'Virginia',
+        'wa' => 'Washington',
+        'wv' => 'West Virginia',
+        'wi' => 'Wisconsin',
+        'wy' => 'Wyoming',
+    );
+
+    // The next few functions are ugly, because they have to work in PHP 5.2...
+    // for everyone who doesn't have to support 5.2, please, for the love, make
+    // your ViewModel return closures rather than `array($this, '...')`
+    //
+    // :)
+
+    public function upcase()
+    {
+        return array($this, '_upcase');
+    }
+
+    public function _upcase($val)
+    {
+        return strtoupper($val);
+    }
+
+    public function eachPair()
+    {
+        return array($this, '_eachPair');
+    }
+
+    public function _eachPair($val)
+    {
+        $ret = array();
+        foreach ($val as $key => $value) {
+            array_push($ret, compact('key', 'value'));
+        }
+
+        return $ret;
+    }
+}
