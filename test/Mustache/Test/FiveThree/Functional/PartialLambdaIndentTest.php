131cad6024515158006dd2942e89f6ad82322aa7
Justin Hileman <justin@justinhileman.info>
2016-02-13 15:36:35 -0800

    Add a failing test for #286

--- test/Mustache/Test/FiveThree/Functional/PartialLambdaIndentTest.php
+++ test/Mustache/Test/FiveThree/Functional/PartialLambdaIndentTest.php
@@ -44,6 +44,36 @@ EOS;
         $data = new Mustache_Test_FiveThree_Functional_ClassWithLambda();
         $this->assertEquals($expected, $tpl->render($data));
     }
+
+    public function testLambdaInterpolationsInsidePartialsAreIndentedProperly()
+    {
+        $src = <<<EOS
+<fieldset>
+  {{> input }}
+</fieldset>
+
+EOS;
+        $partial = <<<EOS
+<input placeholder="{{ placeholder }}">
+
+EOS;
+
+        $expected = <<<EOS
+<fieldset>
+  <input placeholder="Enter your name">
+</fieldset>
+
+EOS;
+
+        $m = new Mustache_Engine(array(
+            'partials' => array('input' => $partial),
+        ));
+
+        $tpl = $m->loadTemplate($src);
+
+        $data = new Mustache_Test_FiveThree_Functional_ClassWithLambda();
+        $this->assertEquals($expected, $tpl->render($data));
+    }
 }
 
 class Mustache_Test_FiveThree_Functional_ClassWithLambda
@@ -52,6 +82,13 @@ class Mustache_Test_FiveThree_Functional_ClassWithLambda
     {
         return function ($val) {
             return strtoupper($val);
+        };
+    }
+
+    public function placeholder()
+    {
+        return function() {
+            return 'Enter your name';
         };
     }
 }
